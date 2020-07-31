614822e9cf0a5b825bd3d5256249b6f8b58a03c0
YeongG <yeonggil223@gmail.com>
2020-08-01 01:22:23 +0900

    tsc index.ts [useForms]

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 "use strict";
 exports.__esModule = true;
-exports.useInput = void 0;
+exports.useForm = exports.useInput = void 0;
 var react_1 = require("react");
 exports.useInput = function (initialValue) {
     var _a = react_1.useState(initialValue || ""), inputValue = _a[0], setInputValue = _a[1];
@@ -9,3 +9,22 @@ exports.useInput = function (initialValue) {
     }, []);
     return [inputValue, setInputValue, changeInputValue];
 };
+var isEmpty = function (stats) {
+    var boolean = stats.reduce(function (value, state) { return value || !!state === false; }, false);
+    return boolean;
+};
+exports.useForm = function (stats, option) {
+    var submitFunc = react_1.useCallback(function (e) {
+        var require = option.require, requireFunc = option.requireFunc, submitFunc = option.submitFunc, preventDefault = option.preventDefault;
+        if (preventDefault)
+            e.preventDefault();
+        if (require && isEmpty(stats)) {
+            requireFunc();
+            return;
+        }
+        submitFunc();
+    }, stats);
+    return submitFunc;
+};
+// ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+// useForm
